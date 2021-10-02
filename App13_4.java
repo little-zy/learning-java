256822386b6bdfcff9b4e4336bde583e36f57624
little-zy <91786171+little-zy@users.noreply.github.com>
2021-10-03 00:04:35 +0800

    Add files via upload

---
+++ App13_4.java
@@ -0,0 +1,22 @@
+
+//filename：App13_4.java
+public class App13_4
+{
+  public static void main(String[] args)
+  {
+    (
+      new Inner()
+      {
+        void setName(String n)
+        {
+          name=n;
+          System.out.println("姓名："+name);
+        }
+      }
+    ).setName("张  华");
+  }
+  static class Inner
+  {
+    String name;
+  }
+}
---
+++ scanner_in.java
@@ -0,0 +1,18 @@
+
+
+//filename：App3_6.java
+import java.util.*;
+
+public class scanner_in {
+  public static void main(String[] args) {
+    int num1;
+    double num2;
+    Scanner reader = new Scanner(System.in);
+    System.out.print("请输入第一个数：");
+    num1 = reader.nextInt();
+    System.out.print("请输入第二个数：");
+    num2 = reader.nextDouble();
+    System.out.println(num1 + "*" + num2 + "=" + ((float) num1 * num2));
+    reader.close();
+  }
+}
